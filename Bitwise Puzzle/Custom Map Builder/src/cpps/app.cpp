812fdc39a70606b1b989ec3703311e6c9ac75c2e
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2023-03-18 15:44:26 +0200

    Make tile selector

--- Bitwise Puzzle/Custom Map Builder/src/cpps/app.cpp
+++ Bitwise Puzzle/Custom Map Builder/src/cpps/app.cpp
@@ -42,7 +42,30 @@ void App::initWindow() {
 		}
 	}
 
-	this->window.create(VideoMode(1280, 720), "Custom Map Builder");
+	this->selectorBorder.setSize(Vector2f(80,80));
+	this->selectorBorder.setOutlineThickness(-3);
+	this->selectorBorder.setFillColor(Color::Transparent);
+	this->selectorBorder.setOutlineColor(Color::White);
+	this->selectorBorder.setPosition(1330, 20);
+
+	this->selectorVoid.setTexture(this->voidTexture);
+	this->selectorWall.setTexture(this->wallTexture);
+	this->selectorFloor.setTexture(this->floorTexture);
+
+	this->selectorVoid.setPosition(1330, 20);
+	this->selectorWall.setPosition(1330, 120);
+	this->selectorFloor.setPosition(1330, 220);
+
+	this->saveButton.setPosition(1300, 630);
+	this->saveButton.setSize(Vector2f(140, 60));
+	this->saveButton.setOutlineThickness(2);
+	this->saveButton.setOutlineColor(Color::White);
+	this->saveButton.setFillColor(Color(54, 201, 115));
+
+
+	this->currentTile = 0;
+
+	this->window.create(VideoMode(1460, 720), "Custom Map Builder");
 	updateWindow();
 }
 
@@ -62,13 +85,21 @@ void App::updateWindow() {
 }
 
 void App::renderWindow() {
-	this->window.clear();
+	this->window.clear(Color(60,60,60));
 
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 16; j++) {
 			this->window.draw(tiles[i][j]);
 		}
 	}
+
+	this->window.draw(this->selectorVoid);
+	this->window.draw(this->selectorWall);
+	this->window.draw(this->selectorFloor);
+
+	this->window.draw(this->selectorBorder);
+
+	this->window.draw(this->saveButton);
 
 	this->window.display();
 }
@@ -78,28 +109,52 @@ void App::handleInput() {
 		float x = event.mouseButton.x;
 		float y = event.mouseButton.y;
 
-		int tileX = x / 80;
-		int tileY = y / 80;
+		if (x < 1280) {
+			int tileX = x / 80;
+			int tileY = y / 80;
 
-		std::cout << tileX << " " << tileY << std::endl;
+			this->map[tileY][tileX] = this->currentTile;
+			this->tiles[tileY][tileX].setTexture(textureMap[map[tileY][tileX]]);
+		}
+		else {
+			Vector2f voidStartPos(this->selectorVoid.getPosition().x, this->selectorVoid.getPosition().y);
+			Vector2f voidEndPos(this->selectorVoid.getPosition().x + 80, this->selectorVoid.getPosition().y + 80);
 
-		this->map[tileY][tileX] = 2;
-		this->tiles[tileY][tileX].setTexture(textureMap[map[tileY][tileX]]);
+			Vector2f wallStartPos(this->selectorWall.getPosition().x, this->selectorWall.getPosition().y);
+			Vector2f wallEndPos(this->selectorWall.getPosition().x + 80, this->selectorWall.getPosition().y + 80);
+
+			Vector2f floorStartPos(this->selectorFloor.getPosition().x, this->selectorFloor.getPosition().y);
+			Vector2f floorEndPos(this->selectorFloor.getPosition().x + 80, this->selectorFloor.getPosition().y + 80);
+
+			if (x >= voidStartPos.x && x <= voidEndPos.x && y >= voidStartPos.y && y <= voidEndPos.y) {
+				this->selectorBorder.setPosition(Vector2f(1330, 20));
+				this->currentTile = 0;
+			}
+			else if (x >= wallStartPos.x && x <= wallEndPos.x && y >= wallStartPos.y && y <= wallEndPos.y) {
+				this->selectorBorder.setPosition(Vector2f(1330, 120));
+				this->currentTile = 1;
+
+			}
+			else if (x >= floorStartPos.x && x <= floorEndPos.x && y >= floorStartPos.y && y <= floorEndPos.y) {
+				this->selectorBorder.setPosition(Vector2f(1330, 220));
+				this->currentTile = 2;
+
+			}
+		}
 	}
 }
 
+void App::saveLevel() {
+	_json["2"] = json::array();
 
+	for (int i = 0; i < 9; i++) {
+		int row[16] = {};
+		for (int j = 0; j < 16; j++) {
+			row[j] = map[i][j];
+		}
 
-
-/* _json["2"] = json::array();
-
-for (int i = 0; i < 9; i++) {
-	int row[16] = {};
-	for (int j = 0; j < 16; j++) {
-		row[j] = map[i][j];
+		_json["2"].push_back(row);
 	}
 
-	_json["2"].push_back(row);
+	file << _json.dump(4, ' ');
 }
-
-file << _json.dump(4, ' '); */
--- Bitwise Puzzle/Custom Map Builder/src/includes/app.hpp
+++ Bitwise Puzzle/Custom Map Builder/src/includes/app.hpp
@@ -6,12 +6,19 @@ public:
 	void updateWindow();
 	void renderWindow();
 	void handleInput();
+	void saveLevel();
 private:
+	int currentTile;
 	RenderWindow window;
 	Event event;
-	Texture wallTexture;
 	Texture voidTexture;
+	Texture wallTexture;
 	Texture floorTexture;
+	Sprite selectorVoid;
+	Sprite selectorWall;
+	Sprite selectorFloor;
+	RectangleShape selectorBorder;
+	RectangleShape saveButton;
 	std::map<int, Texture> textureMap;
 	std::vector<std::vector<int>> map;
 	std::vector<std::vector<Sprite>> tiles;
